45f551891ea16ae4a53e8971af3dcc667ba95637
Emmie <36717379+Emmie8@users.noreply.github.com>
2022-09-28 23:54:42 -0500

    [App] Initial Commit

---
+++ App.style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#14A0DB',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: '420',
+        color: 'white',
+    },
+    button: {
+        fontSize: 25,
+        textAlign: 'center',
+        margin: 7,
+        color: 'white',
+        backgroundColor: "#491FBD",
+        borderRadius: 5,
+    },
+});
+  
