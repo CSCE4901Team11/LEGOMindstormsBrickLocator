ad98b1acaa2df07b84b1aed9d246cdc5f06b5ab7
dlk0138 <dlk0138@my.unt.edu>
2022-10-09 18:01:27 -0500

    Initial dev commit. Begin work on edge detection

--- App.js
+++ App.js
@@ -1,64 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Pressable, TouchableOpacity } from 'react-native';
-import styles from './App.style.js';
-import React from 'react';
-import { Camera } from 'expo-camera';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Pressable, TouchableOpacity } from "react-native";
+import styles from "./App.style.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Camera } from "expo-camera";
+
+function ObjectDetection() {
+  return (
+    <Camera
+      style={{ flex: 1, width: "100%" }}
+    >
+      
+    </Camera>
+  );
+}
 
 export default function App() {
-  const [startCamera,setStartCamera] = React.useState(false)
-
+  const [startCamera, setStartCamera] = React.useState(false);
   const __startCamera = async () => {
-    const {status} = await Camera.requestCameraPermissionsAsync()
-    if (status === 'granted') {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === "granted") {
       // start the camera
-      setStartCamera(true)
+      setStartCamera(true);
     } else {
-      Alert.alert('Access denied')
+      Alert.alert("Access denied");
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Life is pain</Text>
       <Pressable style={styles.button}>
         {({ pressed }) => (
-          <Text style={styles.button}>
-            {pressed ? 'Bruh' : 'Press Me'}
-          </Text>
+          <Text style={styles.button}>{pressed ? "Bruh" : "Press Me"}</Text>
         )}
       </Pressable>
-      
-      {startCamera ? (
-        <Camera
-          style={{flex: 1,width:"100%"}}
-          ref={(r) => {
-            camera = r
-          }}
-        ></Camera>
-      ) : (
 
-          <TouchableOpacity
-            onPress={__startCamera}
+      {startCamera ? (
+        <ObjectDetection />
+      ) : (
+        <TouchableOpacity
+          onPress={__startCamera}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: "#14274e",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: 40,
+          }}
+        >
+          <Text
             style={{
-              width: 130,
-              borderRadius: 4,
-              backgroundColor: '#14274e',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 40
+              color: "#fff",
+              fontWeight: "bold",
+              textAlign: "center",
             }}
           >
-            <Text
-              style={{
-                color: '#fff',
-                fontWeight: 'bold',
-                textAlign: 'center'
-              }}
-            >
-              Camera
-            </Text>
-          </TouchableOpacity>
+            Camera
+          </Text>
+        </TouchableOpacity>
       )}
 
       <StatusBar style="auto" />
