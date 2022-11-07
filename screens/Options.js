09a3d3dd89f2aa537ad3bc395907720fd92b8944
Emmie <36717379+Emmie8@users.noreply.github.com>
2022-11-07 14:27:39 -0600

    Add Extra Navigation

--- screens/Options.js
+++ screens/Options.js
@@ -1,9 +1,10 @@
+import { useNavigation } from '@react-navigation/native';
 import { useContext } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, } from 'react-native';
 import { ThemeContext } from '../constants/context';
 import styles from './Options.styles';
 
-const OptionsScreen = ({ navigation }) => {
+const OptionsScreen = () => {
 
   const currentTheme = useContext (ThemeContext);
   const theme = currentTheme.state.theme;
@@ -11,6 +12,8 @@ const OptionsScreen = ({ navigation }) => {
   const blueThemeChange = ()  => {
           currentTheme.dispatch({ type: "BLUEMODE" })
   };
+
+  const navigation = useNavigation();
 
     return (
       <View style={ theme == 'light' ? styles.container_light : theme == 'dark' ? styles.container_dark : styles.container_blue }>
@@ -36,62 +39,15 @@ const OptionsScreen = ({ navigation }) => {
            <Text style={styles.headerTwoText}>More</Text> 
            <Button
             title='About us'
-          />
-          <Button
+            onPress={() => navigation.navigate('OptionsNav', {screen: 'About'} )}
+            />
+            <Button
             title='Privacy policy'
-          />
-           </View>
+            />
+        </View>
        
       </View>
     );
   }
-
-  const OptionStyles = StyleSheet.create({
-    container: {
-    flex: 1,
-    padding: 10,
-    //backgroundColor: "#eaeaea"
-  },
-
-  headerText: {
-    fontWeight: 'bold',
-    fontSize: 35,
-    marginTop: 30,
-    paddingVertical: 13,
-    textAlign: "center",
-    padding: 4,
-    //flex: 1,
-    //padding: 10,
-    //borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  headerTwoText: {
-    fontWeight: 'bold',
-    marginTop: 300,
-    paddingVertical: 10,
-    textAlign: 'center',
-    fontSize: 20,
-    color: 'black'
-  },
-
-  headerRow: {
-    //padding: 10,
-    borderBottomColor: "black",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-
-  button: {
-    marginTop: 10,
- //   paddingVertical: 8,
-    borderWidth: 1,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#d2d4d9",
-  //  color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
-  },
-
-});
 
 export default OptionsScreen;
