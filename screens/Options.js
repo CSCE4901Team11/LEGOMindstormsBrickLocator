54424c42307f7317f1b50822742253e505ec737f
Mya-Miller <101530032+Mya-Miller@users.noreply.github.com>
2022-11-12 21:16:36 -0600

    about us modal (#14)

--- screens/Options.js
+++ screens/Options.js
@@ -1,11 +1,17 @@
-
-import { useContext } from 'react';
-import { View, Text, Button, } from 'react-native';
+import { useContext, useState } from 'react';
+import { View, Text, Button, StyleSheet, Alert, Pressable} from 'react-native';
+import Modal from "react-native-modal";
 import { ThemeContext } from '../constants/context';
 import styles from './Options.styles';
 
+const OptionsScreen = ({ navigation }) => {
+ // const [modalVisible, setModalVisible] = useState(false);
 
-const OptionsScreen = ({navigation}) => {
+ const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
   const currentTheme = useContext (ThemeContext);
   const theme = currentTheme.state.theme;
@@ -34,20 +40,33 @@ const OptionsScreen = ({navigation}) => {
           />
           </View>
         </View>
-        <View style={styles.container}>
-            <Text style={styles.headerTwoText}>More</Text> 
-            <Button
-              title='About us'
-              onPress={() => navigation.navigate('AboutUs')} // despite the documentation, call the screen name and not the navigator function then the screen name
-            />
-            <Button
+        <View style={styles.space}>
+        <View>
+      <Button title="About Us" onPress={toggleModal} color="black" />
+
+      <Modal isVisible={isModalVisible}>
+        <View style={{ margin: 0 }}>
+          <Text style={{backgroundColor: "white", borderRadius: 20, padding: 35, alignItems: "center", justifyContent: 'space-evenly', shadowOffset: { width: 0, height: 2}, shadowOpacity: 0.25, shadowRadius: 4,elevation: 5}}>The LEGO® Mindstorms® kits are LEGO® kits that help users ages 10 
+          and up learn how to build and program robots. These kits come with come with many different pieces and it can be hard 
+          for users to differentiate these pieces. The purpose of this project is to create an application that can help users 
+          identify and locate any piece in the LEGO® Education Spike™ kits when using the provided organization tray. Creating this 
+          application will allow for easier completion of Spike™ robots as well as improve the accessibility of the LEGO® Education 
+          Spike™ kits for users who are visually impaired.</Text>
+          <Button title="Close" onPress={toggleModal} color="white"/>
+        </View>
+      </Modal>
+    </View>
+        <View style={styles.headerTwoText}>
+          <Button
               title='Privacy policy'
               onPress={() => navigation. navigate ('PrivacyPolicy')}
             />
-        </View>
-       
-      </View>
+          </View>
+          </View>
+         </View>
     );
+
+
   }
 
-export default OptionsScreen;
+
