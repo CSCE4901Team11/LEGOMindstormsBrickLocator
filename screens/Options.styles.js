ddcd7453cdd31a8ba9953d1718e3ba9ca4d18746
Mya-Miller <101530032+Mya-Miller@users.noreply.github.com>
2022-11-12 21:00:23 -0600

    about us modal

--- screens/Options.styles.js
+++ screens/Options.styles.js
@@ -14,7 +14,7 @@ export default styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: "#121212"
     },
-
+ 
     container_blue: {
         flex: 1,
         alignItems: 'center', 
@@ -51,31 +51,103 @@ export default styles = StyleSheet.create({
     //borderBottomWidth: StyleSheet.hairlineWidth
   },
   headerTwoText: {
-    fontWeight: 'bold',
-    marginTop: 300,
+   // fontWeight: 'bold',
+   // marginTop: 300,
     paddingVertical: 10,
     textAlign: 'center',
     fontSize: 20,
+   // margin: 10,
+   // padding: 15,
     color: 'black'
   },
 
+  space: {
+    padding: 45,
+  },
+
   headerRow: {
+    //borderBottomColor: "black",
+    //borderBottomWidth: StyleSheet.hairlineWidth,
     //padding: 10,
-    borderBottomColor: "black",
-    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 
   button: {
+    fontSize: 10,
+    textAlign: 'center',
+    margin: 12,
+    color: 'rgba(0,0,0,0.8)',
+    backgroundColor: 'rgba(189,195,199, .9)',
+    borderRadius: 5,
+    padding: 10,
+    /*
     marginTop: 10,
- //   paddingVertical: 8,
     borderWidth: 1,
     borderColor: "#20232a",
     borderRadius: 6,
     backgroundColor: "#d2d4d9",
-  //  color: "#20232a",
     textAlign: "center",
     fontSize: 30,
     fontWeight: "bold"
+    paddingVertical: 8,
+    color: "#20232a",
+    */
+
+    centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'space-evenly',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  /*
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: 'rgba(189,195,199, .9)',
+  },
+  buttonClose: {
+    backgroundColor: 'rgba(189,195,199, .9)',
+  },
+  textStyle: {
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  */
+  button2: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonClose: {
+    backgroundColor: 'rgba(189,195,199, .9)',
+    textAlign: "center"
+  },
+  modalText: {
+   // marginBottom: 15,
+    textAlign: "center",
+    margin: 0,
+  }
   },
 
 });
