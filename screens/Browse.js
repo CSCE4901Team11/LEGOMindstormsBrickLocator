d62350e5ed051681fcb83bd7a139f29308b5fdf9
lorenekeeland <lorenekeeland@my.unt.edu>
2022-11-14 01:30:52 -0600

    Typescript to Browse Class
    Removing the typescript and making it a class within browse

--- screens/Browse.js
+++ screens/Browse.js
@@ -1,9 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { View, Text, Pressable, TouchableOpacity , TextInput, Button } from 'react-native';
 import { ThemeContext } from '../constants/context';
 import styles from './Browse.styles';
 import SearchBar from 'react-native-dynamic-search-bar';
 import GetPieces from '../components/Pieces';
+import staticData from './staticData';
+
+export default class Search extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            query: '',
+            dataBackup: staticData,
+            dataSource: staticData,
+        };
+    }
+
+    filterList = (text) => {
+        var newData = this.state.dataBackup,
+        newData = this.state.dataBackup.filter((item) => {
+            const itemData = item.name.toLowercase();
+            const testData = text.toLowercase();
+            return itemData.indexOf(textData) > -1;
+        });
+        this.setState({
+            query:text,
+            dataSource: newData,
+        });
+    };
+
+    renderItem(item){
+        return(
+            <View style={ theme == 'light' ? styles.container_light : theme == 'dark' ? styles.container_dark : styles.container_blue }> //make background style for results
+                key={item.name}
+                title={item.name}
+                imageSource={item.image}
+                subtitle={item.color}
+            </View>
+        );
+    };
+
+}
 
 function BrowseScreen () {
 
