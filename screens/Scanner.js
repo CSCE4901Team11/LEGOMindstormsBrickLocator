fa25d9a8d7e6b81488158c4a348a760df582304a
dlk0138 <dlk0138@my.unt.edu>
2022-11-15 04:32:26 -0600

    Changed tensorflow model to cocoSSD.

--- screens/Scanner.js
+++ screens/Scanner.js
@@ -1,4 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+export default ScannerScreen;
+
+
 import { Text, View, Pressable, TouchableOpacity, Alert } from 'react-native';
 import styles from './Scanner.styles';
 import React, { useContext, useEffect, useState } from 'react';
@@ -6,10 +8,9 @@ import { Camera } from 'expo-camera';
 import { ThemeContext } from '../constants/context';
 import { useNavigation } from '@react-navigation/native';
 
-import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as cocoSSD from '@tensorflow-models/coco-ssd';
 import * as tf from '@tensorflow/tfjs';
 import * as tfrn from '@tensorflow/tfjs-react-native'
-import { withRepeat } from 'react-native-reanimated';
 
 const TensorCamera = tfrn.cameraWithTensors(Camera);
 
@@ -20,7 +21,9 @@ function ScannerScreen() {
   const [hasPermission, setHasPermission] = useState(false);
   const [brickDetected, setBrickDetected] = useState(false);
 
-  const [mobilenetModel, setMobilenetModel] = useState(null); //Replace with custom model
+  const cameraRef = useRef(null);
+
+  const [cocoSSDModel, setcocoSSDModel] = useState(null); //Replace with custom model
   const [frameworkReady, setFrameworkReady] = useState(false);
 
   const navigation = useNavigation();
@@ -29,15 +32,13 @@ function ScannerScreen() {
 
   let requestAnimationFrameId = 0;
 
+  let x = 0;
+  let y = 0;
+  let w = 0;
+  let h = 0;
+
   const textureDims = { width: 1080, height: 1920 };
-  const tensorDims = { width: textureDims.width/4, height: textureDims.height/4 }; 
-
-  /*
-  const highlight = document.createElement('div');
-    highlight.setAttribute('class', 'highlight');
-    highlight.style = 'left: ' + prediction[n].bbox[0] + 'px; top: ' + prediction[n].bbox[1] + 'px; width: ' + prediction[n].bbox[2] + 'px; height: ' + prediction[n].bbox[3]+ 'px;';
-
-    */
+  const tensorDims = { width: textureDims.width/4, height: textureDims.height/4 };     
 
   useEffect(() => {
     if(!frameworkReady) {
@@ -53,7 +54,7 @@ function ScannerScreen() {
 
         console.log(`tf status: ${tf.ready()}`)
         //load the mobilenet model and save it in state
-        setMobilenetModel(await loadMobileNetModel());
+        setcocoSSDModel(await loadcocoSSDModel());
 
         console.log(`tf ready`)
         setFrameworkReady(true);
@@ -71,14 +72,12 @@ function ScannerScreen() {
   const DetectBrick = async (className) => { //To Implement
     setBrickDetected(true);
     loadNewBrick()
-    return(
-      <Text className />
-    );
+    drawBoundingBoxes(x,y,w,h);
   }
 
-  const loadMobileNetModel = async () => {
+  const loadcocoSSDModel = async () => {
     console.log('Start loading model');
-    const model = await mobilenet.load();
+    const model = await cocoSSD.load();
     console.log(`model loaded`);
     return model;
   }
@@ -87,18 +86,27 @@ function ScannerScreen() {
     if(!tensor) { return; }
 
     //topk set to 1
-    const prediction = await mobilenetModel.classify(tensor, 1);
-    console.log(`prediction: ${JSON.stringify(prediction)}`);
+    const prediction = await cocoSSDModel.detect(tensor);
+    if(prediction != 'undefined' || prediction != '[]') {
+      console.log(`prediction: ${JSON.stringify(prediction)}`);
+    }
 
     if(!prediction || prediction.length === 0) { return; }
     
     //only attempt detection when confidence is higher than 20%
-    if(prediction[0].probability > 0.5) {
+    if(prediction[0].score > 0.5) {
+
+      x = prediction[0].bbox[0];
+      y = prediction[0].bbox[1];
+      w = prediction[0].bbox[2];
+      h = prediction[0].bbox[3];
 
       cancelAnimationFrame(requestAnimationFrameId);
       setPredictionFound(true);
 
       await DetectBrick(prediction[0].className);
+
+      drawBoundingBoxes(x,y,w,h);
     }
   }
 
@@ -107,12 +115,23 @@ function ScannerScreen() {
   return;
   }
 
+  const drawBoundingBoxes = (x,y,w,h) => {
+    console.log("Drawing bounding boxes.")
+    return(
+      <View style={[styles.highlighter, {top: y-10,left: x-10,width: w*2,height: h*2, zIndex: 9999}]}>
+        <Text>???????????????</Text>
+      </View>
+    );
+  }
+
   const handleCameraStream = (imageAsTensors) => {
     const loop = async () => {
       if(!frameworkReady) {await delay();}
       const nextImageTensor = await imageAsTensors.next().value;
       await getPrediction(nextImageTensor);
+      drawBoundingBoxes(x,y,w,h);
       requestAnimationFrameId = requestAnimationFrame(loop);
+      tf.dispose(imageAsTensors);
     };
     if(!predictionFound) loop();
   }
@@ -125,22 +144,20 @@ function ScannerScreen() {
 
   const renderCameraView = (frameworkReady) => {
     return <View flex={1}>
-                <TensorCamera
-                  style={styles.camera_window}
-                  type={Camera.Constants.Type.back}
-                  zoom={0}
-                  cameraTextureHeight={textureDims.height}
-                  cameraTextureWidth={textureDims.width}
-                  resizeHeight={tensorDims.height}
-                  resizeWidth={tensorDims.width}
-                  resizeDepth={3}
-                  onReady={imageAsTensors => handleCameraStream(imageAsTensors)}
-                  autorender={true} >
-                    <Text style={theme == 'light' ? styles.text_light : theme == 'dark' ? styles.text_dark : styles.text_blue}>
-                    Point to a brick to identify it.
-                    </Text>
-                  </TensorCamera>
-            </View>;
+        <TensorCamera
+          style={styles.camera_window}
+          type={Camera.Constants.Type.back}
+          ref={cameraRef}
+          zoom={0}
+          cameraTextureHeight={textureDims.height}
+          cameraTextureWidth={textureDims.width}
+          resizeHeight={tensorDims.height}
+          resizeWidth={tensorDims.width}
+          resizeDepth={3}
+          onReady={imageAsTensors => handleCameraStream(imageAsTensors)}
+          autorender={true} >
+          </TensorCamera>
+    </View>;
   }
 
   const awaitFrameworkReady = () => {
@@ -152,9 +169,6 @@ function ScannerScreen() {
   return (
       <View style={theme == 'light' ? styles.container_light : theme == 'dark' ? styles.container_dark : styles.container_blue}>
         {awaitFrameworkReady()}
-        <StatusBar style="auto" />
       </View>
   );
 }
-
-export default ScannerScreen;
