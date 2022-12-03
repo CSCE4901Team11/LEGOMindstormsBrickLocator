0b22b41beff785af03644a93905c879c32b21b8c
Emmie Abels <36717379+Emmie8@users.noreply.github.com>
2022-12-03 16:52:02 -0600

    Updated piece data, added basic search functionality, and changed start screen (#22)
    * [App] Added React Navigation
    
    * [App] Change to Drawer Navigation
    
    * Update menu
    
    * [App] Updated side menu
    
    Updated side menu to include app title, dark mode switch, and hid screen name and header background
    
    * [App] Add Dark mode
    
    * [App] Updated Dark Mode and added multi-theme capabilities
    
    * [App] Update Dark Mode and add Blue Mode
    
    * Update Scanner screen and theme colors
    
    * Update text color
    
    * Add Extra Navigation
    
    * Update Options.js
    
    * Update Options Navigation
    
    * Fix nested navigator calls
    
    * Added Search Bar
    
    * Added API call to view pieces
    
    * Updated Theme implementation
    
    * Updated Select Piece function
    
    * Updated package.json and added clear user data function
    
    * Added missing imports
    
    * Update theme implementation
    
    * Fixed Theme Switch
    
    * Add piece info json and update browse screen
    
    * Added basic search functionality
    
    * Changed start screen to browse screen
    
    * add backup json to branch
    
    * Compatibility test

--- screens/Browse.js
+++ screens/Browse.js
@@ -1,25 +1,82 @@
-import { useContext } from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { Text, View, TouchableHighlight, FlatList, Image, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import SearchBar from 'react-native-dynamic-search-bar';
 import { ThemeContext } from '../constants/context';
 import styles from './Browse.styles';
-import SearchBar from 'react-native-dynamic-search-bar';
-import GetPieces from '../components/Pieces';
 import Themes from '../constants/ThemeColors';
+import pieces from '../constants/MasterPartList.json';
 
 function BrowseScreen () {
 
     const currentTheme = useContext (ThemeContext)
     const theme = currentTheme.state.theme
     const colors = Themes[theme]
+    //const [filterText, setFilterText] = useState()
+    const [filteredData, setFilteredData] = useState(() => (pieces.Parts))
+
+    const setData = () => {
+        setFilteredData(pieces.Parts)
+    }
+    
+    const filterData = (text) => {
+        const regex = new RegExp(text, "i")
+        var data = (pieces.Parts).filter((item) => {
+            return regex.test(item.Official_Name)
+        })
+        //console.log(data)
+        setFilteredData(data)
+    }
+
+    const listSeparator = () => {
+        return <View style={styles.listSeparator} />;
+    }
+
+    const navigation = useNavigation();
+    const SelectPiece = async (name) => {
+        try {
+            await AsyncStorage.setItem('@current_piece', name)
+            navigation.navigate('Scanner')
+            console.log("piece saved")
+        }
+        catch (error) {
+            console.log('piece not saved')
+        }
+    }
+
+    const renderItem = ({ item }) => {
+        
+        return (
+            <View style={[styles.itemContainer, {backgroundColor: colors.backgroundColor} ]}>
+                <Image style = {styles.image} source={{uri: item.Image}} />
+                <View style={styles.itemInfoContainer}>
+                    <Text style = {[styles.itemTitle, {color: colors.textColor}]}>{item.Official_Name}</Text>
+                    <Text style = {[styles.text, {color: colors.textColor}]}>Element ID/Design ID: {item.Sheet_Element_ID}/{item.Main_Part_ID}</Text>
+                    <Text style = {[styles.text, {color: colors.textColor}]}>Color: {item.Color} </Text>
+                </View>
+                <TouchableOpacity onPress={() => SelectPiece(item.Official_Name) }>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>Select Piece</Text>
+                </View>
+            </TouchableOpacity>
+            </View>
+        )
+    }
 
     return (
         <View style={[styles.container, {backgroundColor: colors.background}] }>
          <SearchBar
             placeholder="Search here"
-            onPress={() => alert("onPress")}
-            onChangeText={(text) => console.log(text)}
+            onClearPress={() => setData()}
+            onChangeText={(text) => {filterData(text), console.log(text)}} 
          />
-         <GetPieces />
+         <FlatList style={styles.list}
+                data={filteredData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.ID}
+                ItemSeparatorComponent={listSeparator}
+            />
         </View>
     );
 }
