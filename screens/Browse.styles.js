d3cfadf14c5af1c95d2ad08ed385a278109c8882
lorenekeeland <lorenekeeland@my.unt.edu>
2022-12-04 16:52:48 -0600

    whitespace change to browse screen

--- screens/Browse.styles.js
+++ screens/Browse.styles.js
@@ -6,7 +6,7 @@ export default styles = StyleSheet.create({
         flex: 1, 
         alignItems: 'center', 
         justifyContent: 'center',
-        //marginTop: 100,
+        marginTop: 50,
     },
       title: {
         fontSize: 35,
